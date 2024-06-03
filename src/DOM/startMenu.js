dc39519b60b37d72ffd8a572ba2e6817b87a916b
citronoiseau <avroraka2004@gmail.com>
2024-06-03 21:01:06 +0300

    add styling for starting menu

--- src/DOM/startMenu.js
+++ src/DOM/startMenu.js
@@ -8,16 +8,21 @@ const content = document.querySelector("#content");
 
 export default function startMenu() {
   const startMenuContainer = document.createElement("div");
+  startMenuContainer.classList.add("startMenuContainer");
   content.appendChild(startMenuContainer);
 
   const titleContainer = document.createElement("div");
-
+  titleContainer.classList.add("titleContainer");
   const title = document.createElement("div");
   title.classList.add("title");
-  title.textContent = "PaperBoats";
+  title.textContent = "Battleship";
   titleContainer.appendChild(title);
 
   startMenuContainer.appendChild(titleContainer);
+
+  const buttonContainer = document.createElement("div");
+  buttonContainer.classList.add("buttonContainer");
+  startMenuContainer.appendChild(buttonContainer);
 
   const startGameButtonContainer = document.createElement("div");
   createInitializeGameButton(startGameButtonContainer);
@@ -28,9 +33,20 @@ export default function startMenu() {
   const gameStyleButtonContainer = document.createElement("div");
   createGameStyleChangeButton(gameStyleButtonContainer);
 
-  startMenuContainer.appendChild(startGameButtonContainer);
-  startMenuContainer.appendChild(gameModeButtonContainer);
-  startMenuContainer.appendChild(gameStyleButtonContainer);
+  const message = document.createElement("div");
+  message.textContent = "by ";
+  message.classList.add("authorMessage");
+  const authorLink = document.createElement("a");
+  authorLink.textContent = "Anastasiia";
+  authorLink.href = "https://github.com/citronoiseau/odin-battleship";
+  authorLink.target = "_blank";
+
+  message.appendChild(authorLink);
+
+  buttonContainer.appendChild(startGameButtonContainer);
+  buttonContainer.appendChild(gameModeButtonContainer);
+  buttonContainer.appendChild(gameStyleButtonContainer);
+  startMenuContainer.appendChild(message);
 
   return startMenuContainer;
 }

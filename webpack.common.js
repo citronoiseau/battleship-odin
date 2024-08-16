d7b971180b7e770f86235765ec712e74845ac812
Asia <134224668+citronoiseau@users.noreply.github.com>
2024-08-15 21:00:36 -0700

    fixed the problem with icon not displaying in dist

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -7,6 +8,9 @@ module.exports = {
     new HtmlWebpackPlugin({
       title: "Battleship",
       template: "./src/index.html",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: "src/icons/rudder.png", to: "icons/rudder.png" }],
     }),
   ],
   output: {
